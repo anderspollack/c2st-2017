5d507dd8ee487022c27604819b606e3351c014ec
anderspollack <pollack.anders@gmail.com>
2018-04-02 00:36:40 -0700

    Fix issue with display of content type label

--- template-parts/content-library.php
+++ template-parts/content-library.php
@@ -6,25 +6,22 @@
  *
  * @package C2ST_2017
  */
-$terms = get_the_terms($post, 'content_type_taxonomy');
+// $terms = get_the_terms($post, 'content_type_taxonomy');
 // add label to account for display on single initiative pages
-if ( 'event' === get_post_type() ) :
-    $item_type = get_post_type_object( get_post_type() ) -> labels -> singular_name;
-else :
-    if (get_field('content_type_taxonomy')) {
-        $item_type = get_field('content_type_taxonomy') -> name;
-    }
+if ( 'event' === get_post_type() ): $item_type = get_post_type_object( get_post_type() ) -> labels -> singular_name;
+else:
+    if (get_field('content_type_taxonomy')): $item_type = get_field('content_type_taxonomy') -> name;
+    else: $item_type = 'Post';
+    endif;
 endif; 
 ?>
 <div class="col-sm-6">
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <header class="entry-header">
         
-            <?php 
-            if ($terms) {
-                echo '<span class="feature-label">' . $item_type . '</span>';
-            }
+            <span class="feature-label"><?php echo $item_type ?></span>
 
+            <?php 
             the_title( '<h3 class="content-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 
             if ( 'post' === get_post_type() ) : ?>
@@ -36,26 +33,37 @@ endif;
         </header><!-- .entry-header -->
 
         <?php 
-        // Post Thumbnail
-        if ( has_post_thumbnail() &&  'Video' !== get_field( 'content_type_taxonomy' ) -> name ) : ?>
         
-            <a href="<?php echo esc_url( get_permalink() ); ?>" class="content-image" 
-            style="background-image: url('<?php esc_url( the_post_thumbnail_url() ); ?>');">
-            </a>
-
-        <?php 
+        // if ( has_post_thumbnail() &&  'Video' !== (get_field( 'content_type_taxonomy' ) -> name) ) :
         // Embedded Video Player
-        elseif ('Video' === get_field( 'content_type_taxonomy') -> name) : 
-            $video_url = get_field( 'youtube_video_url' );
-            $code_pos = strrpos( $video_url, 'watch?v=' );
-            $embed_video_url = substr_replace( $video_url, 'embed/', $code_pos, 8 )
-         ?>
+        if (get_field('content_type_taxonomy')):
+            if ('Video' === get_field( 'content_type_taxonomy') -> name): 
+                if (get_field('youtube_video_url')): 
+                    $video_url = get_field( 'youtube_video_url' );
+                    $code_pos = strrpos( $video_url, 'watch?v=' );
+                    $embed_video_url = substr_replace( $video_url, 'embed/', $code_pos, 8 );
+                    ?>
 
-            <div class="video-container">
+                    <div class="video-container">
+                        <iframe class="video" src="<?php echo $embed_video_url; ?>" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    
+                <?php endif; ?>
 
-                <iframe class="video" src="<?php echo $embed_video_url; ?>" frameborder="0" allowfullscreen></iframe>
+                
 
-            </div>
+            <?php 
+            // Post Thumbnail for non-Video posts
+            elseif (has_post_thumbnail()): ?>
+
+                <a href="<?php echo esc_url( get_permalink() ); ?>" class="content-image" style="background-image: url('<?php esc_url( the_post_thumbnail_url() ); ?>');"></a>
+
+            <?php 
+            endif;
+        // Post Thumbnail for Events
+        elseif (has_post_thumbnail()) : ?>
+
+            <a href="<?php echo esc_url( get_permalink() ); ?>" class="content-image" style="background-image: url('<?php esc_url( the_post_thumbnail_url() ); ?>');"></a>
 
         <?php 
         endif; ?>
