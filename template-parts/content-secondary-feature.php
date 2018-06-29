aaef8a4137932cc9fc30f6dd4eca30c433b9dcf4
anderspollack <pollack.anders@gmail.com>
2018-06-29 14:09:43 -0700

    Give Featured Initiatives icons

--- template-parts/content-secondary-feature.php
+++ template-parts/content-secondary-feature.php
@@ -1,20 +1,23 @@
 <?php 
 if ( get_post_type() === 'event' ) {
-    $featured_glyphicon = 'glyphicon-fire';
-    $featured_item_type = get_post_type_object( get_post_type() );
-    $featured_item_type = $featured_item_type -> labels -> singular_name;
+  $featured_glyphicon = 'glyphicon-fire';
+  $featured_item_type = get_post_type_object( get_post_type() );
+  $featured_item_type = $featured_item_type -> labels -> singular_name;
 } else if ( get_field( 'content_type' ) === 'Blog Post' ) {
-    $featured_glyphicon = 'glyphicon-text-background';
-    $featured_item_type = get_field( 'content_type' );
+  $featured_glyphicon = 'glyphicon-text-background';
+  $featured_item_type = get_field( 'content_type' );
 } else if ( get_field( 'content_type' ) === 'Video' ) {
-    $featured_glyphicon = 'glyphicon-film';
-    $featured_item_type = get_field( 'content_type' );
+  $featured_glyphicon = 'glyphicon-film';
+  $featured_item_type = get_field( 'content_type' );
 } else if ( get_post_type() === 'give_forms' ) {
-    $featured_glyphicon = 'glyphicon-gift';
-    $featured_item_type = 'Donation Initiative';
+  $featured_glyphicon = 'glyphicon-gift';
+  $featured_item_type = 'Donation Initiative';
+} else if ( get_post_type() === 'initiative' ) {
+  $featured_glyphicon = 'glyphicon-tree-conifer';
+  $featured_item_type = 'Initiative';
 } else {
-    $featured_glyphicon = 'glyphicon-bullhorn';
-    $featured_item_type = get_field( 'content_type' );
+  $featured_glyphicon = 'glyphicon-bullhorn';
+  $featured_item_type = get_field( 'content_type' );
 } ?>
         
 <div class="col-sm-6 col-md-6 col-lg-6">
