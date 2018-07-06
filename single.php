19cc96888bf7d776185ccfef3dc3433aecb3d44d
anderspollack <pollack.anders@gmail.com>
2018-07-06 09:33:35 -0700

    Enable Comments

--- single.php
+++ single.php
@@ -17,17 +17,15 @@ get_header(); ?>
                     <?php
                     while ( have_posts() ) : the_post();
 
-                        get_template_part( 'template-parts/content', get_post_format() );
+                    get_template_part( 'template-parts/content', get_post_format() );
 
                         // Previous and Next Links for Posts
                         // the_post_navigation();
 
                         // If comments are open or we have at least one comment, load up the comment template.
-                        /*
-                        if ( comments_open() || get_comments_number() ) :
-                            comments_template();
-                        endif;
-                        */
+                    
+                    comments_template();
+                    
 
                     endwhile; // End of the loop.
                     ?>
