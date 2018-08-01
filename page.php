f488cca59ebd0813e02939396ac36602637ddfcb
anderspollack <pollack.anders@gmail.com>
2018-07-31 20:55:00 -0700

    Update default page template for Give Donation receipts

--- page.php
+++ page.php
@@ -14,25 +14,20 @@
 
 get_header(); ?>
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div class="container">
 
             <?php
             while ( have_posts() ) : the_post();
 
-                get_template_part( 'template-parts/content', 'page' );
-
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
+            get_template_part( 'template-parts/content', 'page' );
 
             endwhile; // End of the loop.
             ?>
-
-        </main><!-- #main -->
-    </div><!-- #primary -->
+        </div>
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
