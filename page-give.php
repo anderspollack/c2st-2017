401fb7dbc8bd9855f6302a9f1f32cd4ccb39816b
anderspollack <pollack.anders@gmail.com>
2018-08-21 15:46:10 -0700

    update single give pages to include Guidestar seal

--- page-give.php
+++ page-give.php
@@ -8,8 +8,8 @@ $page_title = get_the_title(); ?>
 
 <?php 
 /* 
-Give Page Primary Features
-*/
+   Give Page Primary Features
+ */
 $primary_featured_post = get_field( 'primary_featured_post' );
 if ( $primary_featured_post ) : ?>
 
@@ -20,27 +20,35 @@ if ( $primary_featured_post ) : ?>
     // get_template_part( 'template-parts/content', 'primary-feature' );
     // wp_reset_postdata(); ?>
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main give-now" role="main">
-        <div class="page-section">
-            <div class="container">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main give-now" role="main">
+            <div class="page-section">
+                <div class="container">
 
-                <h1 class="entry-title"><?php echo $page_title; ?></h1>
+                    <div class="donation-header">
+                        <h1 class="entry-title"><?php echo $page_title; ?></h1>
 
-                <?php
-                echo do_shortcode( '[give_form id="' . $post . '"]' ); ?>
+                        <div class="guidestar">
+                            <a href="https://www.guidestar.org/profile/20-8490697" target="_blank">
+                                <img src="<?php echo get_theme_file_uri('img/gximage2.png')?>" />
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <?php
+                    echo do_shortcode( '[give_form id="' . $post . '"]' ); ?>
 
-            </div><!-- .container -->
-        </div><!-- .page-section -->
-    </main><!-- #main -->
-</div><!-- #primary -->
+                </div><!-- .container -->
+            </div><!-- .page-section -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php endif; ?>
 
 <!-- <script type="text/javascript">
-    // adds column break CSS to the parent 'p' of 'span.column-break'
-    // jQuery(".column-break").parents('p').css("break-after", "column");
-</script> -->
+     // adds column break CSS to the parent 'p' of 'span.column-break'
+     // jQuery(".column-break").parents('p').css("break-after", "column");
+     </script> -->
 
 <?php
 // get_sidebar();
