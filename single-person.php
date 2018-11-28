9de39682476f3b876007fd9dc783788b55dd334f
anderspollack <pollack.anders@gmail.com>
2018-11-27 23:55:40 -0800

    add container to single person pages

--- single-person.php
+++ single-person.php
@@ -9,7 +9,9 @@ get_header(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
-            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <div class="container">
+                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            </div><!-- .container -->
         </main><!-- #main -->
     </div><!-- #primary -->
 </article>
