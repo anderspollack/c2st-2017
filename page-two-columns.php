a3771fbade52fb95df6ff0a33cb27785e8371183
anderspollack <pollack.anders@gmail.com>
2019-02-27 22:56:29 -0800

    update two column page template

--- page-two-columns.php
+++ page-two-columns.php
@@ -20,25 +20,59 @@ get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
         <div class="container">
-
-            <div class="row">
-                <div class="col-sm-6">
-                    <?php
-                    while ( have_posts() ) : the_post();
-
-                    get_template_part( 'template-parts/content', 'page' );
-
-                    endwhile; // End of the loop.
-                    ?>
-                </div>
-                <?php if (get_field( 'second_column' )) : ?>
-                    <div class="col-sm-6">
-                        <?php the_field( 'second_column' ); ?>
-                        ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <header class="entry-header">
+                            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                        </header><!-- .entry-header -->
                     </div>
-                <?php endif; ?>
-            </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="entry-content">
+                            <?php
+                            the_content();
 
+                            wp_link_pages( array(
+                              'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'c2st-2017' ),
+                              'after'  => '</div>',
+                            ) );
+                            ?>
+                        </div><!-- .entry-content -->
+
+                        <?php if ( get_edit_post_link() ) : ?>
+                            <footer class="entry-footer">
+                                <?php
+                                edit_post_link(
+                                  sprintf(
+                                    wp_kses(
+                                      /* translators: %s: Name of current post. Only visible to screen readers */
+                                      __( 'Edit <span class="screen-reader-text">%s</span>', 'c2st-2017' ),
+                                      array(
+                                        'span' => array(
+                                          'class' => array(),
+                                        ),
+                                      )
+                                    ),
+                                    get_the_title()
+                                  ),
+                                  '<span class="edit-link">',
+                                  '</span>'
+                                );
+                                ?>
+                            </footer><!-- .entry-footer -->
+                        <?php endif; ?>
+                    </div>
+                    <div class="col-sm-6">
+                        <?php if (get_field( 'second_column' )) : ?>
+                            <div class="col-sm-6">
+                                <?php the_field( 'second_column' ); ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </article><!-- #post-<?php the_ID(); ?> -->
         </div>
     </main><!-- #main -->
 </div><!-- #primary -->
