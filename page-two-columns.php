49bf475cc60f79fb92810c6074825a1322f2a0a4
anderspollack <pollack.anders@gmail.com>
2019-02-27 23:01:15 -0800

    fix two column layout images

--- page-two-columns.php
+++ page-two-columns.php
@@ -17,7 +17,7 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="content-area layout-two-columns">
     <main id="main" class="site-main" role="main">
         <div class="container">
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
