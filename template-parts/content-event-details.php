c6124c03cf60041d89637020ad375233a29b16b1
Anders Pollack <pollack.anders@gmail.com>
2019-09-03 14:47:19 -0400

    adds eventbrite option linking to one event

--- template-parts/content-event-details.php
+++ template-parts/content-event-details.php
@@ -87,7 +87,34 @@ if ( get_field( 'address' ) ) : ?>
 
 <?php if ( get_field( 'registration_url' ) ): ?>
 
-    <p class="content-subheading"><a href="<?php the_field( 'registration_url' ); ?>" target="_blank" class="btn btn-primary"><?php the_field( 'registration_button_text' ); ?></a></p>
+    <p class="content-subheading">
+        <?php if ( get_field( 'is_eventbrite' ) ) : ?>
+            <!-- Embedded eventbrite form -->
+            <!-- Noscript content for added SEO -->
+            <noscript><a href="https://www.eventbrite.com/e/urban-nature-walk-tickets-70703049725" rel="noopener noreferrer" target="_blank"></noscript>
+                <!-- You can customize this button any way you like -->
+                <button id="eventbrite-widget-modal-trigger-70703049725" type="button" class="btn btn-primary"><?php the_field( 'registration_button_text' ); ?></button>
+                <noscript></a>Buy Tickets on Eventbrite</noscript>
+
+                <script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
+
+                <script type="text/javascript">
+                var exampleCallback = function() {
+                    console.log('Order complete!');
+                };
+
+                window.EBWidgets.createWidget({
+                    widgetType: 'checkout',
+                    eventId: '70703049725',
+                    modal: true,
+                    modalTriggerElementId: 'eventbrite-widget-modal-trigger-70703049725',
+                    onOrderComplete: exampleCallback
+                });
+                </script>
+        <?php else : ?>
+            <a href="<?php the_field( 'registration_url' ); ?>" target="_blank" class="btn btn-primary"><?php the_field( 'registration_button_text' ); ?></a>
+        <?php endif; ?>
+    </p>
 
 <?php endif; ?>
 
